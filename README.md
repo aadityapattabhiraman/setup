340bd2380f330e5838fb61b9013086d7f596d761
aadityapattabhiraman <aadityapattabhiraman47@proton.me>
2024-09-11 03:48:13 +0000

    awesome

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # setup
  
+### awesome.sh
+This file is a install script to install my awesome configuration.  
+one can just download the file using
--- awesome.sh
+++ awesome.sh
@@ -18,6 +18,21 @@ if [ -z "$aur_manager" ]; then
   exit 1
 fi
 
-packages=("package1" "package2" "package3")
+packages=("rofi" "picom" "i3lock-fancy" "xclip" "ttf-roboto" "polkit-gnome" "materia-theme" "lxappearance" "flameshot" "pnmixer" "network-manager-applet" "xfce4-power-manager" "qt5-styleplugins" "papirus-icon-theme" "git")
+yes | $aur_manager -S "${packages[@]}" -y
 
-$aur_manager -S "${packages[@]}"
+wget https://archive.archlinux.org/packages/a/awesome/awesome-4.3-3-x86_64.pkg.tar.zst
+yes | sudo pacman -U awesome-4.3-3-x86_64.pkg.tar.zst
+rm awesome-4.3-3-x86_64.pkg.tar.zst
+
+echo "Packages installed successfully. Cloning repository..."
+git clone https://github.com/aadityapattabhiraman/awesomwm ~/.config/awesome
+
+echo "Repository cloned successfully. Configuring Rofi..."
+mkdir -p ~/.config/rofi
+cp $HOME/.config/awesome/theme/config.rasi ~/.config/rofi/config.rasi
+sed -i '/@import/c\@import "'$HOME'/.config/awesome/theme/sidebar.rasi"' ~/.config/rofi/config.rasi
+
+echo "Same theme for Qt/KDE applications and GTK applications, and fix missing indicators"
+echo "XDG_CURRENT_DESKTOP=Unity" >> /etc/environment
+echo "QT_QPA_PLATFORMTHEME=gtk2" >> /etc/environment
